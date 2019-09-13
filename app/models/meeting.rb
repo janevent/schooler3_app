57143b937fd334ec0065200a2c668a4b89d96182
Jane Venter <janeanneventer@gmail.com>
2019-09-13 09:43:23 -0700

    write in Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,8 @@
 class Meeting < ApplicationRecord
+    belongs_to :student
+    belongs_to :course
+    belongs_to :user, through: :student
+    validates :day, inclusion: { in %w(sunday monday tuesday wednesday thursday friday saturday), message: "%{value} is not a day"}
+    #validates start_times and end_times to be a time
+    scope: monday => {where(day: monday)}
 end
