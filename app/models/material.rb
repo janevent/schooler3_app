b9ce6a773341ac8030f4090894a38bcdf171f36e
Jane Venter <janeanneventer@gmail.com>
2019-09-13 09:48:38 -0700

    write in associations, validation and scope method

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,6 @@
 class Material < ApplicationRecord
+    has_many :course_materials
+    has_many :courses, through: :course_materials
+    validates :item, presence: true
+    scope: obtained => {where('obtained = ?', true)}
 end
