310a58e1e174307e572b5041e8e941c55c7da0f4
Jane Venter <janeanneventer@gmail.com>
2019-09-13 09:26:04 -0700

    add validations and has_secure_password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :students
-    has_many :courses, through: :students
+    has_many :meetings, through: :students
+    has_secure_password
+    validates :name, presence: true
+    validates :email, confirmation: true
+    validates :email_confirmation, presence: true
 end
