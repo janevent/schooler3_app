5b8624fba218805608847fa33fa8cd30ccab2b5c
Jane Venter <janeanneventer@gmail.com>
2019-09-14 21:23:03 -0700

    write helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+    helper_methods :current_user, :logged_in?
+    
     def home 
+    end
+
+    private
+
+    def current_user 
+        @current_user ||= session.find_by(session[:id]) if session[:id]
+    end
+
+    def logged_in?
+        !!current_user
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, confirmation: true
     validates :email_confirmation, presence: true
+
+    
 end
