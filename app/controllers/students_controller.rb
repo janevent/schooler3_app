ce7cbd29aabeae99e123a972a20f0aab7c95c9c3
Jane Venter <janeanneventer@gmail.com>
2019-09-15 22:03:37 -0700

    write create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,20 @@
 class StudentsController < ApplicationController
     def new
         @user = User.find_by(id: current_user[:user_id])
-        @student = @user.students.new
+        @student = @user.students.build #create
     end
 
     def create
+        @user = User.find_by(id: current_user[:user_id])
+        @student = @user.students.build(student_params)
+        if current_user && @student.save
+            redirect_to user_student_path(@student)
+        elsif current_user
+            redirect_to user_new_student_path
+        else
+            redirect_to home_path 
+        end
+
     end
 
     def show 
