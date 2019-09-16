e06f1716d695f69d0fd5d81111e7f79b17f45ab6
Jane Venter <janeanneventer@gmail.com>
2019-09-15 22:15:03 -0700

    update resources to include nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get '/', to: 'application#home', as: 'home'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit,:update. :delete] do 
+    resources :students
+  end
 
   get '/login', to: 'sessions#new'
   post '/users', to: 'sessions#create'
