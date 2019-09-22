5def1e41fd52d8fa5056abba43120fe277cc5cb6
Jane Venter <janeanneventer@gmail.com>
2019-09-21 21:07:42 -0700

    add index view and course routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
 
   get '/', to: 'application#home', as: 'home'
+  
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do 
-    resources :students
+    resources :students do 
+      resources :courses
+    end
   end
 
   get '/login', to: 'sessions#new'
   post '/users', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  
+  #resources :courses
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
