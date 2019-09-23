8c60fe7c0c08ab78a4fc8ed22e642709580ac9ba
Jane Venter <janeanneventer@gmail.com>
2019-09-23 10:04:30 -0700

    change index

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -4,11 +4,7 @@ class Materialscontroller < ApplicationController
 
     def index
         @user = User.find_by(id: current_user[:user_id])
-        materials = @user.students.each do |student| 
-            student.courses.each do |course|
-                course.materials
-            end
-        end
-        @materials = materials
+        @materials = Material.all #how to access a users materials
+        #users-students-enrollments-courses-course_materials -materials
     end
 end
