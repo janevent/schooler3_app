87983b1afb445e6a13b3fa5101a8d763eda0c9f3
Jane Venter <janeanneventer@gmail.com>
2019-09-26 11:08:21 -0700

    write in actions

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,9 +1,23 @@
 class DaysController < ApplicationController
 
     def new 
+        @course = Course.find_by(id: params[:course_id])
+        7.times do 
+            @day = @course.build.days 
+            3.times do 
+                @meeting = @day.build.meetings 
+            end
+        end
     end
 
     def create 
+        @course = Course.find_by(id: params[:course_id])
+        @day = Day.new(day_params)
+        if @day.save 
+            redirect_to course_days_path(@course)
+        else 
+            render :new
+        end 
     end
 
     def index 
